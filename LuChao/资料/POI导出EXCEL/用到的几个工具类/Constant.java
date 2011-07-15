cb7035b8d4862a5dac1cda07c478a35aa36c39e7
zhlx2835@sina.com <zhlx2835@sina.com@4f103aba-f866-4a7b-6b16-2355d66fe423>
2011-07-15 05:56:15 +0000

    添加 内容 资料

---
+++ LuChao/资料/POI导出EXCEL/用到的几个工具类/Constant.java
@@ -0,0 +1,10 @@
+package com.dpsms.util;
+
+public class Constant {
+	
+	public static final String TEMPLATES_PARTY_BASIC_ALL_YX_COMPANISE_TEMPLATES = "com/dpsms/poireport/temp.xls";
+	public static final String TEMPLATES_PARTY_BASIC_ALL_ZDDDFX_COMPANISE_TEMPLATES = "com/dpsms/poireport/zdddfx.xls";
+	public static final String TEMPLATES_PARTY_BASIC_ALL_YHZLCX_COMPANISE_TEMPLATES = "com/dpsms/poireport/yhzlcx.xls";
+	
+ 
+}
